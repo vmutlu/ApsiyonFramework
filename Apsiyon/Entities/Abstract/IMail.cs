da1556f9876c406c14149d739563a921d3663441
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-09-24 22:18:29 +0300

    Interfaces have been created for the properties to be used for mail sending.

---
+++ Apsiyon/Entities/Abstract/IMail.cs
@@ -0,0 +1,10 @@
+﻿namespace Apsiyon.Entities.Abstract
+{
+    public interface IMail
+    {
+        string Address { get; set; }
+        string Subject { get; set; }
+        string Message { get; set; }
+        bool IsBody { get; set; }
+    }
+}
---
+++ Apsiyon/Entities/Abstract/ISmtp.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Mail;
+
+namespace Apsiyon.Entities.Abstract
+{
+    public interface ISmtp
+    {
+        string Mail { get; set; }
+        string Name { get; set; }
+        string Host { get; set; }
+        bool EnableSSL { get; set; }
+        int Port { get; set; }
+        string Password { get; set; }
+        bool UseDefaultCredentials { get; set; }
+        SmtpDeliveryMethod DeliveryMethod { get; set; }
+    }
+}
